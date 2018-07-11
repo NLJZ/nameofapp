806b658a4c4af1ee30af099abf091f0363cef439
Nathan <johnson.nathan.lee@gmail.com>
2018-07-11 13:48:32 +0200

    4.2 orders controller added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  root 'simple_pages#index'
-  
+  root 'simple_pages#landing_page'
+  resources :orders, only: [:index, :show, :create, :destroy]
 end
