804c85ec251d2cca205fe60f49cd9e2a885ffbc3
Nathan <johnson.nathan.lee@gmail.com>
2018-07-23 15:14:33 +0200

    5.1 updated routes and products_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :products
+  get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  root 'simple_pages#landing_page'
+  get 'simple_pages/orders'
+  root 'simple_pages#index'
   resources :orders, only: [:index, :show, :create, :destroy]
 end
