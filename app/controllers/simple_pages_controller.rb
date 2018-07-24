65113239d931c7aa11f43db3aa3c3b2a7e98011d
Nathan <johnson.nathan.lee@gmail.com>
2018-07-24 14:05:03 +0200

    5.3 contact form added

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -5,4 +5,15 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'johnson.nathan.lee@gmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/orders'
   get 'simple_pages/landing_page'
+  post 'simple_pages/thank_you'
   root 'simple_pages#index'
   
   resources :orders, only: [:index, :show, :create, :destroy]
