899bb10d7fb4797d6cae0d8b83fd51d0da738596
Nathan <johnson.nathan.lee@gmail.com>
2018-07-25 11:45:44 +0200

    change orders total field to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_134217) do
+ActiveRecord::Schema.define(version: 2018_07_25_094154) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
     t.integer "product_id"
-    t.float "total"
+    t.integer "total"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
