3b773a5d4bf9f62a8b84722ff0422b9d21c4e357
Nathan <johnson.nathan.lee@gmail.com>
2018-07-26 17:52:40 +0200

    comments model update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :products
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
@@ -9,6 +8,10 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page'
   post 'simple_pages/thank_you'
   root 'simple_pages#index'
-  
+
+  resources :products do
+    resources :comments
+  end
+
   resources :orders, only: [:index, :show, :create, :destroy]
 end
