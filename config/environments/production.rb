1aa8014deb86130474ae1a70f7bac14c5f68165b
Nathan <johnson.nathan.lee@gmail.com>
2018-08-14 11:30:16 +0200

    add realtime action cable new comment alert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'bikesx3.herokuapp.com' }
+
+  config.web_socket_server_url = "wss://bikesx3.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://bikesx3.herokuapp.com', 'http://bikesx3.herokuapp.com']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:index, :show, :create, :destroy]
+
+  mount ActionCable.server => '/cable'
+  
 end
